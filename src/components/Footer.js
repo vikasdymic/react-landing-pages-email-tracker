7c1a623b6e91461737a746f0ba158b3eba54fab7
Dymic Digital <tech@dymic.com>
2019-06-19 15:08:44 -0700

    added modal window

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,20 @@ import './Footer.css';
 import Button from './Button';
 import verisign from '../img/verisign.png'
 import norton from '../img/nortonverified.png'
+import Modal from './Modal';
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isOpen: false };
+  }
+
+  toggleModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
       <footer>
@@ -13,7 +25,10 @@ class Footer extends Component {
             <img className='verisign' src={verisign} alt="Verisign trusted" />
             <img className='verisign' src={norton} alt="Verisign trusted" />
           </div>
-          <Button buttonStyle='primary' buttonLabel='Install Email Tracker' />
+          {/* <Button buttonStyle='primary' buttonLabel='Install Email Tracker' /> */}
+          <button className='primary button' onClick={this.toggleModal}>Install Email Tracker</button>
+          <Modal show={this.state.isOpen}
+            onClose={this.toggleModal}>Email Tracker</Modal>
         </div>
         <div className='footerBottom'>
           <p>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import './Hero.css';
 import Button from './Button';
 import sunandpuddleslogo from '../img/sunandpuddleslogo.png';
-import star from '../img/star.png'
+import star from '../img/star.png';
+import Modal from './Modal';
 
 class Hero extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isOpen: false };
+  }
+
+  toggleModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
       <section className='heroSection'>
@@ -16,7 +28,10 @@ class Hero extends Component {
           <p className='heroContentCopy'>
             Know who is opening and reading your emails with Email Tracker. Simply install Email Tracker and be alerted to <i>anyone</i> who opens or reads your emails.
           </p>
-          <Button buttonLabel='Download Today' buttonStyle='hero' />
+          {/* <Button buttonLabel='Download Today' buttonStyle='hero' /> */}
+          <button className='hero button' onClick={this.toggleModal}>Download Today</button>
+          <Modal show={this.state.isOpen}
+            onClose={this.toggleModal}>Email Tracker</Modal>
           <div className='starRating'>
             <img src={star} alt='star' />
             <img src={star} alt='star' />
