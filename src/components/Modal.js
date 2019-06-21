18ef5f5c6fdef052416d3874b89c6c7dc80c3482
Vikas Bhavsar <vikas@dymic.com>
2019-06-21 12:54:54 -0700

    added form

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,6 +13,10 @@ class Modal extends Component {
                 <div id='modal'>
                     <h2>Coming Soon!</h2>
                     <p>Thank you for expressing your interest in {this.props.children}! We are putting the finishing touches on {this.props.children} and you'll be the first to know when it's done.</p>
+                    <form name="email" data-netlify="true">
+                        <input className="input" type="email" placeholder="email"/>
+                        <input className="button small" type="submit" value="Submit" required/>
+                    </form>
                     <button class='button medium' onClick={this.props.onClose}>Close</button>
                 </div>
             </div>
